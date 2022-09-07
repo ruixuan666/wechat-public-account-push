8ae0ea92d2001337b97953a07c9fdaa5ef254eeb
ruixuan666 <112825883+ruixuan666@users.noreply.github.com>
2022-09-07 22:41:02 +0100

    Update index.js

--- config/index.js
+++ config/index.js
@@ -31,7 +31,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oWOe753tED2qY3JfIlgKGGLKVraM",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "wMZE1Gk2aOpP5aVMgKc5Y5rME5mFXlRS6f7ZYE3VL0Q",
+      useTemplateId: "qbhRVtTuDX9ab1bUfHElmsqrgw-crE84UA7KbK9EmsY",
       // 所在省份
       province: "甘肃",
       // 所在城市
